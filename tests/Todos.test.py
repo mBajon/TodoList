45a8acb40ae8723713315b1500be41636a548143
mbajon <maciej.bajon@wp.pl>
2018-07-01 23:00:45 +0200

    solved import issues

--- tests/Todos.test.py
+++ tests/Todos.test.py
@@ -1,2 +1,11 @@
 import unittest
-from app.Todos import Todo
+import app.Todos
+#from ..Todos import Todos
+
+class Todos_test(unittest.TestCase):
+    TestTodo=app.Todos.Todos()
+    #assert(isinstance(TestTodo,Todos),True)
+    
+
+if __name__ == '__main__':
+    unittest.main()
